e2fcff535531e2afbe4e610f31f256688dca1fc0
lejan22 <alejandrodl2206@gmail.com>
2022-04-11 10:45:37 +0200

    gameplayfixes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
     public float speed= 10f  ;
     public float impulse = 5F;
     public float groundDistance = 2f;
@@ -11,10 +12,11 @@ public class PlayerController : MonoBehaviour
     public float velocityMax = 100f;
 
     public float mouseSensitivity = 100f;
-
+    private float spinspeed = 200f;
     private GameObject focalPoint;
     private float horizontalInput, verticalInput;
     private Rigidbody playerRigidbody;
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -22,18 +24,42 @@ public class PlayerController : MonoBehaviour
 
         focalPoint = GameObject.Find("FocalPoint");
         Physics.gravity *= 2;
-        playerRigidbody.velocity = focalPoint.transform.forward * 200f;
+        //playerRigidbody.velocity = focalPoint.transform.forward * 200f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Jump 
         if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
         {
             playerRigidbody.AddForce(Vector3.up * impulse, ForceMode.Impulse);
         }
-
-     
+        //Boost en el aire
+        if (Input.GetKeyDown(KeyCode.Space) && !IsOnGround())
+        {
+            playerRigidbody.velocity = focalPoint.transform.forward * 50f;
+            
+        }
+         //Spindash
+         //Rotar sobre si mismo
+         if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            
+            transform.Rotate(Vector3.left * spinspeed * Time.deltaTime);
+        }
+         //Sale disparado hacia adelante respecto a la camara
+        if (Input.GetKeyUp(KeyCode.Mouse0) && IsOnGround())
+        {
+            playerRigidbody.velocity = focalPoint.transform.forward * 100f;
+            
+        }
+        //ground pound
+      if (Input.GetKeyUp(KeyCode.Mouse0) && !IsOnGround())
+        {
+            playerRigidbody.AddForce(Vector3.down * 100f, ForceMode.Impulse);
+         
+        }
     }
 
     private void FixedUpdate()
@@ -52,6 +78,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+   
     private bool IsOnGround()
     {
         RaycastHit hitData;
@@ -69,4 +96,5 @@ public class PlayerController : MonoBehaviour
             return false;
         }
     }
+    
 }
