d0f7c1b2a3a0d7d33c4d9538855e3f5025aa857a
Tomeu de la Parte <tomeudelapartemulet@gmail.com>
2022-05-06 13:50:16 +0200

    Pendulo y otros

---
+++ Assets/Scripts/PlayerSprite.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprite : MonoBehaviour
+{
+    private GameObject theCam;
+    private GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        theCam = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.LookAt(theCam.transform);
+
+        transform.position = (Player.transform.position + Vector3.up * 3);
+    }
+}
