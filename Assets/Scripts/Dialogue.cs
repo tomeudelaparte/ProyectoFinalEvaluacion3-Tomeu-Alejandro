237429c8e68eeb55977ebc0813e209c01cbcb146
lejan22 <91455326+lejan22@users.noreply.github.com>
2022-05-30 21:36:27 +0200

    New circuit

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Dialogue : MonoBehaviour
+{
+    
+    public Canvas canvas;
+ /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            Debug.Log($"it works!");
+        }
+
+
+        
+    }
+ */
+}
